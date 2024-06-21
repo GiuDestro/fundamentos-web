f04386ab8ea0dfb48f4f90a465876fd617172755
Giulia Destro <giulia01destrosilva@gmail.com>
2024-06-21 14:19:23 -0300

    Inicio proj blog em react

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return (
+        <>
+            <h1 className="titulo">Home</h1>
+            <img src="https://w7.pngwing.com/pngs/349/467/png-transparent-welcome-ladies-and-gentlemen-youtube-dailymotion-welcome-company-rectangle-wood-thumbnail.png" alt="Imagem Tela Inicial" className="img"/>
+        </>
+    );
+}
+
+export default Home;
